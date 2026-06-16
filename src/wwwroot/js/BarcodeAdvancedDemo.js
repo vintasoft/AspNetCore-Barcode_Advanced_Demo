a99c2ca7973dfb0169ad6aa8a7aee9b312e449d3
vintasoft <alex@vintasoft.com>
2026-06-16 12:57:44 +0300

    Source codes update

--- src/wwwroot/js/BarcodeAdvancedDemo.js
+++ src/wwwroot/js/BarcodeAdvancedDemo.js
@@ -136,7 +136,7 @@ function __initMenu(docViewerSettings) {
 
     var uploadAndOpenFileButton = items.getItemByRegisteredId("uploadAndOpenFileButton");
     if (uploadAndOpenFileButton != null)
-        uploadAndOpenFileButton.set_FileExtensionFilter(".bmp, .cur, .doc, .docx, .gif, .ico, .j2k, .j2c, .jb2, .jbig2, .jp2, .jpc, .jpeg, .jpg, .jls, .pbm, .pcx, .pdf, .png, .psd, .tga, .tif, .tiff, .svg, .xls, .xlsx");
+        uploadAndOpenFileButton.set_FileExtensionFilter(".bmp, .cur, .doc, .docx, .gif, .ico, .j2k, .j2c, .jb2, .jbig2, .jp2, .jpc, .jpeg, .jpg, .jls, .pbm, .pcx, .pdf, .png, .psd, .tga, .tif, .tiff, .svg, .xls, .xlsx, .pptx");
 
     // get the main menu of document viewer
     var mainMenu = items.getItemByRegisteredId("mainMenu");
